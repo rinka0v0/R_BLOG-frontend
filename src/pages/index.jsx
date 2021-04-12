0056dfe804dce1c8b5cd2eed6f2aee10bd634590
rinca-layla.love2.0409@docomo.ne.jp <rinka0v0>
2021-04-12 17:49:36 +0900

    pages/index.jsxにルーティングを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,28 @@
 import LinkButton from "../components/LinkButton/index";
 import styles from "../styles/indexPage.module.scss";
 import Image from "next/image";
+import useUser from "../data/useUser";
+import { useEffect } from "react";
+import Router from "next/router";
+import Loading from "../components/Loading";
+
 
 const IndexPage = () => {
+  const { loggedIn, loading } = useUser();
+
+  useEffect(() => {
+    if (loggedIn) {
+      Router.replace("/home");
+    }
+  }, [loggedIn]);
+
+  if (loggedIn) {
+    return <Loading />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className={styles.container}>
         <h1>R_BLOG</h1>
